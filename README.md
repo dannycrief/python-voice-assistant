d54713dea8e64ff63c7a584dc5ffd3313a39a6c7
Stepan Kozurak <step.kozbvb@gmail.com>
2020-11-08 02:05:01 +0100

    Fitst initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Mark Voice Assistant
+
+Mark Voice Assistant is a Python voice assistant project on SpeechRecognition, pyttsx3 and GoogleAPI
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```python
+print("Demo Python code input")
+```
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
---
+++ main.py
@@ -0,0 +1,189 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os
+import time
+import playsound
+import pyttsx3
+import speech_recognition as sr
+import pytz
+import subprocess
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+MONTHS = ['january', 'february', 'march', 'april', 'may', 'june',
+          'july', 'august', 'september', 'october', 'november', 'december']
+DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+DAY_EXTENSIONS = ["nd", "rd", "th", "st"]
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+    return said
+
+
+def authenticate_google():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    return service
+
+
+def get_events(date, service):
+    # Call the Calendar API
+    date = datetime.datetime.combine(date, datetime.datetime.min.time())
+    end_date = datetime.datetime.combine(date, datetime.datetime.max.time())
+    utc = pytz.UTC
+    date = date.astimezone(utc)
+    end_date = end_date.astimezone(utc)
+
+    events_result = service.events().list(calendarId='primary', timeMin=date.isoformat(),
+                                          timeMax=end_date.isoformat(), singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+        speak('No upcoming events found.')
+    else:
+        print(f'You have {len(events)} events on this day')
+        speak(f'You have {len(events)} events on this day')
+
+        for event in events:
+            start = event['start'].get('dateTime', event['start'].get('date'))
+            start_time = str(start.split("T")[1].split("+")[0])
+            if int(start_time.split(":")[0]) < 12:
+                start_time = start_time + "AM"
+            else:
+                start_time = str(int(start_time.split(":")[0]) - 12)
+                start_time = start_time + "PM"
+            print(event["summary"] + " at " + start_time)
+            speak(event["summary"] + " at " + start_time)
+
+
+def get_date(text):
+    text = text.lower()
+    today = datetime.date.today()
+
+    if text.count("today") > 0:
+        return
+
+    day = -1
+    day_of_week = -1
+    month = -1
+    year = today.year
+
+    tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    for word in text.split():
+        if word in MONTHS:
+            month = MONTHS.index(word) + 1
+        elif word in DAYS:
+            day_of_week = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENSIONS:
+                found = word.find(ext)
+                if found > 0:
+                    try:
+                        day = int(word[:found])
+                    except:
+                        pass
+
+    if month < today.month and month != -1:
+        year = year + 1
+
+    if day < today.day and month == -1 and day != -1:
+        month = month + 1
+
+    if month == -1 and day == -1 and day_of_week != -1:
+        current_day_of_week = today.weekday()
+        dif = day_of_week - current_day_of_week
+
+        if dif < 0:
+            dif += 7
+            if text.count("next") >= 1:
+                dif += 7
+
+        return today + datetime.timedelta(dif)
+
+    if text.count("tomorrow") > 0:
+        return tomorrow
+
+    if text.count("yesterday") > 0:
+        return yesterday
+
+    if month == -1 or day == -1:
+        return None
+
+    return datetime.date(month=month, day=day, year=year)
+
+
+def note(text):
+    date = datetime.datetime.now()
+    file_name = str(date).replace(":", "-") + "-note.txt"
+    with open(os.path.join("notes", file_name), "w") as f:
+        f.write(text)
+    subprocess.Popen(["notepad.exe", os.path.join("notes", file_name)])
+
+
+SERVICE = authenticate_google()
+print("Start")
+text = get_audio().lower()
+
+CALENDAR_STRS = ["what do i have", "do i have plans", "am i busy"]
+for phrase in CALENDAR_STRS:
+    if phrase in text:
+        date = get_date(text)
+        if date:
+            get_events(date, SERVICE)
+        else:
+            speak("Please Try Again")
+
+NOTE_STRS = ["make a note", "write this down", "remember this"]
+for phrase in NOTE_STRS:
+    if phrase in text.lower():
+        speak("What would you like me to write down?")
+        note_text = get_audio().lower()
+        note(note_text)
+        speak("I've made a note of that.")
+"""
+# id:
+# 705265415406-84ln1nt38i1s1ru52sst8m9ivejc8pe0.apps.googleusercontent.com
+# secret:
+# _bviOAExrSTdt1HO-wonaGEV
+"""

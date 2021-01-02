681417d42c1f09b6cc4a6d975c5dc3afa1c6ae5b
Stepan Kozurak <step.kozbvb@gmail.com>
2021-01-02 17:07:57 +0100

    Moved files to one folder

---
+++ additional_functions/VA_note.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+import subprocess
+
+
+def note(text):
+    date = datetime.datetime.now()
+    file_name = str(date).replace(":", "-") + "-note.txt"
+    with open(os.path.join("../notes", file_name), "w") as f:
+        f.write(text)
+    subprocess.Popen(["notepad.exe", os.path.join("../notes", file_name)])

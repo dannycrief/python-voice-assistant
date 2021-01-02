c857cd0f1545ac6c9837a3b3ebddf27a7390575a
Stepan Kozurak <step.kozbvb@gmail.com>
2021-01-02 17:10:14 +0100

    Improved functions before starting VA

--- additional_functions/before_start.py
+++ additional_functions/before_start.py
@@ -1,38 +1,56 @@
 import re
-import winreg
 import pickle
+import getpass
 import os.path
-import json
 from random import choices
 
-from VA_config import speak
-from additional_functions.functions import get_installed_programs
+from additional_functions.VA_config import speak, get_speak_engine
+
+ENGINE = get_speak_engine()
 
 
 def get_info_before_begin():
-    speak("Hello, my name is Mark. I am your virtual voice assistant. Give me few minutes to configure settings")
-    print("Mark starts configuration...")
     installed_apps = get_installed_apps_before_begin()
-    print("Collected data about programs")
-    print("Getting some information about user for better work")
     user_info = get_user_info_before_begin()
-    print("Collected data about user")
-    print("Mark ended configuration...")
-    speak("Thank you for choosing me as your own assistant. Let's get started")
     return installed_apps, user_info
+
+
+def get_installed_programs():
+    apps = []
+    path = os.path.join("C:/Users", getpass.getuser(), "AppData/Roaming/Microsoft/Windows/Start Menu/Programs")
+    for root, folders, files in os.walk(path):
+        apps.append({
+            "root": root,
+            "folders": folders,
+            "files": files
+        })
+    path = "C:/ProgramData/Microsoft/Windows/Start Menu/Programs"
+    for root, folders, files in os.walk(path):
+        apps.append({
+            "root": root,
+            "folders": folders,
+            "files": files
+        })
+    for i in apps:
+        for file in i['files']:
+            if file.lower().__contains__('uninstall'):
+                i['files'].remove(file)
+    return apps
 
 
 def get_installed_apps_before_begin():
     installed_apps = None
     if os.path.exists('installed_programs.pickle'):
         with open('installed_programs.pickle', 'rb') as token:
-            installed_apps = json.dumps(pickle.load(token), indent=4, sort_keys=True)
+            installed_apps = pickle.load(token)
     if not installed_apps:
-        installed_apps = get_installed_programs(winreg.HKEY_LOCAL_MACHINE, winreg.KEY_WOW64_32KEY) + \
-                         get_installed_programs(winreg.HKEY_LOCAL_MACHINE, winreg.KEY_WOW64_64KEY) + \
-                         get_installed_programs(winreg.HKEY_CURRENT_USER, 0)
+        speak(ENGINE,
+              "Hello, my name is Mark. I am your virtual voice assistant. Give me few minutes to configure settings")
+        print("Mark starts configuration...")
+        installed_apps = get_installed_programs()
         with open("installed_programs.pickle", "wb") as pickle_file:
             pickle.dump(installed_apps, pickle_file)
+    print("Collected data about programs")
     return installed_apps
 
 
@@ -40,17 +58,19 @@ def get_user_info_before_begin():
     user_info = None
     if os.path.exists('user_info.pickle'):
         with open('user_info.pickle', 'rb') as token:
-            user_info = json.dumps(pickle.load(token), indent=4, sort_keys=True)
+            user_info = pickle.load(token)
+            speak(ENGINE, "Hello {}!".format(user_info['first_name']))
     if not user_info:
-        speak("What is your name?")
+        print("Getting some information about user for better work")
+        speak(ENGINE, "What is your name?")
         first_name = input().capitalize()
-        speak("What is your surname?")
+        speak(ENGINE, "What is your surname?")
         last_name = input().capitalize()
-        speak(f"Nice to meet you {first_name}! I will also need your email for sending your messages")
+        speak(ENGINE, f"Nice to meet you {first_name}! I will also need your email for sending your messages")
         email = input()
         while not is_valid_email(email):
             answers = ["Please give me correct email", "This email is not correct"]
-            speak(choices(answers)[0])
+            speak(ENGINE, choices(answers)[0])
             email = input()
         user_info = {
             "first_name": first_name,
@@ -59,6 +79,8 @@ def get_user_info_before_begin():
         }
         with open("user_info.pickle", "wb") as pickle_file:
             pickle.dump(user_info, pickle_file)
+    print("Collected data about user")
+    print("Mark successfully ended configuration...")
     return user_info
 
 

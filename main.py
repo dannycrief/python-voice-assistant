f81c46fae79f2330c8822c241c4a86470f31ecea
Stepan Kozurak <step.kozbvb@gmail.com>
2021-01-02 17:11:37 +0100

    Added open_program to main.py

--- main.py
+++ main.py
@@ -4,14 +4,19 @@ import random
 import datetime
 import threading
 
-from VA_note import note
-from VA_date import get_date
-from VA_config import speak, get_audio
+from additional_functions.VA_note import note
+from additional_functions.VA_date import get_date
+from additional_functions.before_start import get_info_before_begin
 from googleAPI.googleMaps.google_maps_API import get_google_map_travel
 from googleAPI.googleGmail.google_gmail_API import authenticate_google_gmail
+from additional_functions.VA_config import speak, get_audio, get_speak_engine
 from googleAPI.googleCalendar.google_calendarAPI import authenticate_google_calendar
-from additional_functions.functions import (copy_file, start_browser, execute_math, copy_directory, get_file_path,
-                                            get_directory_path, set_timer)
+from additional_functions.functions import copy_file, start_browser, execute_math, copy_directory, get_file_path, \
+    get_directory_path, set_timer, open_program
+
+ENGINE = get_speak_engine()
+CALENDAR_SERVICE = authenticate_google_calendar()
+GMAIL_SERVICE = authenticate_google_gmail()
 
 WAKE = "hello mark"
 STOP = ["bye", "see you", "goodbye"]
@@ -20,14 +25,14 @@ GMAIL_STRS = ["do i have new messages", "do i have messages"]
 NOTE_STRS = ["make a note", "write this down", "remember this"]
 GMAPS_STRS = ["how can i get", "create a road", "create road", "how long do i need to ride"]
 BROWSER_STRS = ["open browser"]
+OPEN_PROGRAM_STRS = ['run', 'run program', 'open', 'open program', 'start', 'start program', 'launch',
+                     'launch program']
 MATH_STRS = ["add", "plus", "+", "subtract", "minus", "-", "divide", "divided by",
              "/", "multiply", "multiplied by", "times", "*"]
 COPY_STRS = ["copy file", "copy folder", "move file", "move folder"]
 TIME_NOW_STRS = ["current time", "time now", "what time is it"]
 TIMER_STRS = ["set timer"]
 END_STR = ["See you soon!", "Till next time", "Goodbye", "Bye", "See you"]
-CALENDAR_SERVICE = authenticate_google_calendar()
-GMAIL_SERVICE = authenticate_google_gmail()
 
 
 def get_events(date, service):
@@ -44,11 +49,9 @@ def get_events(date, service):
     events = events_result.get('items', [])
 
     if not events:
-        print('No upcoming events found.')
-        speak('No upcoming events found.')
+        speak(ENGINE, 'No upcoming events found.')
     else:
-        print(f'You have {len(events)} events on this day')
-        speak(f'You have {len(events)} events on this day')
+        speak(ENGINE, f'You have {len(events)} events on this day')
 
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
@@ -58,8 +61,7 @@ def get_events(date, service):
             else:
                 start_time = str(int(start_time.split(":")[0]) - 12) + start_time.split(":")[1]
                 start_time = start_time + "PM"
-            print(event["summary"] + " at " + start_time)
-            speak(event["summary"] + " at " + start_time)
+            speak(ENGINE, event["summary"] + " at " + start_time)
 
 
 def get_messages_from_gmail(service):
@@ -69,7 +71,7 @@ def get_messages_from_gmail(service):
     messages = results.get('messages', [])
 
     if not messages:
-        print('No messages found.')
+        speak(ENGINE, 'No messages found.')
     else:
         gmails = []
         messages_count = 0
@@ -78,15 +80,12 @@ def get_messages_from_gmail(service):
             messages_count += 1
             gmails.append(msg)
 
-        print(f"You have {str(messages_count)} unread messages.")
-        speak(f"You have {str(messages_count)} unread messages.")
-        print("Would you like to see your messages: ")
-        speak("Would you like to see your messages: ")
+        speak(ENGINE, f"You have {str(messages_count)} unread messages.")
+        speak(ENGINE, "Would you like to see your messages: ")
         message_choice = get_audio().lower()
 
         if "yes" in message_choice:
-            speak("How many messages you want to display:")
-            print("How many messages you want to display:")
+            speak(ENGINE, "How many messages you want to display:")
             number_of_emails = int(get_audio())
             if number_of_emails == number_of_emails:
                 for gmail in gmails[:number_of_emails]:
@@ -95,13 +94,10 @@ def get_messages_from_gmail(service):
                         name = values["name"]
                         if name == "From":
                             from_name = values["value"]
-                            print(f"You have a new message from: {from_name}")
-                            speak(f"You have a new message from: {from_name}")
-                            print(f"\t{gmail['snippet'][:50]} etc.")
-                            speak(f"{gmail['snippet'][:50]} etc.")
+                            speak(ENGINE, f"You have a new message from: {from_name}")
+                            speak(ENGINE, f"{gmail['snippet'][:50]} etc.")
             else:
-                print("I didn't understand")
-                speak("I didn't understand")
+                speak(ENGINE, "I didn't understand")
 
 
 to_stop = []
@@ -111,7 +107,7 @@ def main():
     while True:
         text = get_audio()
         if text.count(WAKE) > 0:
-            speak("Hello, what do you want me to do?")
+            speak(ENGINE, "Hello, what do you want me to do?")
             text = get_audio()
             for phrase in CALENDAR_STRS:
                 if phrase in text:
@@ -120,14 +116,14 @@ def main():
                         get_events(date, CALENDAR_SERVICE)
                         pass
                     else:
-                        speak("I don't understand")
+                        speak(ENGINE, "I don't understand")
 
             for phrase in NOTE_STRS:
                 if phrase in text:
-                    speak("What would you like me to write down?")
+                    speak(ENGINE, "What would you like me to write down?")
                     note_text = get_audio()
                     note(note_text)
-                    speak("I've made a note of that.")
+                    speak(ENGINE, "I've made a note of that.")
 
             for phrase in GMAIL_STRS:
                 if phrase in text:
@@ -135,87 +131,88 @@ def main():
 
             for phrase in GMAPS_STRS:
                 if phrase in text:
-                    speak("Type your current address. Address first, city (optional) and postal code (optional)")
+                    speak(ENGINE,
+                          "Type your current address. Address first, city (optional) and postal code (optional)")
                     user_address_from = input("Type your current address (address, <city>, <postal code>): ")
-                    speak("Where you want to go (address, city)")
+                    speak(ENGINE, "Where you want to go (address, city)")
                     user_address_to = input("Where you want to go (address, city): ")
                     road_data = get_google_map_travel(user_address_from, user_address_to)
                     if road_data:
                         travel_duration = road_data[0]
                         travel_options = road_data[1]
-                        speak(
-                            f"Your travel will take a {travel_duration}. Do you want me to tell you travel options?")
+                        speak(ENGINE,
+                              f"Your travel will take a {travel_duration}. Do you want me to tell you travel options?")
                         if get_audio() == "yes" or input("Do you want me to tell you travel options? "):
                             for option in travel_options:
-                                print(option)
-                                speak(option)
+                                speak(ENGINE, option)
                         else:
-                            speak("Ok")
-                            print("Ok")
+                            speak(ENGINE, "Ok")
 
             for phrase in BROWSER_STRS:
                 if phrase in text:
-                    print("Which browser?")
-                    speak("Which browser?")
+                    speak(ENGINE, "Which browser?")
                     browser_name = get_audio()
                     open_browser = start_browser(browser_name)
                     if open_browser == "Cannot find this browser":
-                        print(open_browser)
-                        speak(open_browser)
+                        speak(ENGINE, open_browser)
                     else:
-                        print("I've got it. Just a second")
-                        speak("I've got it. Just a second")
+                        speak(ENGINE, "I've got it. Just a second")
                         os.system(open_browser)
 
             for phrase in MATH_STRS:
                 if phrase in text:
                     result = execute_math(phrase, text)
                     if result == "division by zero":
-                        print("Hmm, division by zero is impossible")
-                        speak("Hmm, division by zero is impossible")
+                        speak(ENGINE, "Hmm, division by zero is impossible")
                     else:
-                        print("it is: ", result)
-                        speak(f"it is: {result}")
+                        speak(ENGINE, f"it is: {result}")
 
             for phrase in COPY_STRS:
                 if phrase in text:
                     if "copy file" in phrase:
                         path = get_file_path()
                         if len(path) > 1:
-                            speak("On its way...")
-                            speak("Successfully copied") if copy_file(path[0], path[1]) == "OK" else speak(
+                            speak(ENGINE, "On its way...")
+                            speak(ENGINE, "Successfully copied") if copy_file(path[0], path[1]) == "OK" else speak(
+                                ENGINE,
                                 "Cannot copy file because of error")
                         elif path[0] == "File into file":
-                            speak("Seriously? Do you want to copy file to file? Think about it")
+                            speak(ENGINE, "Seriously? Do you want to copy file to file? Think about it")
                         elif path[0] == "Not a file":
-                            speak("Selected item must be a file, but not folder")
+                            speak(ENGINE, "Selected item must be a file, but not folder")
                     elif "copy folder" in phrase:
                         path = get_directory_path()
                         if len(path) > 1:
-                            speak("On it way...")
-                            speak("Successfully copied") if copy_directory(path[0], path[1]) == "OK" else speak(
-                                "Cannot copy file because of error")
+                            speak(ENGINE, "On it way...")
+                            speak(ENGINE, "Successfully copied") if copy_directory(path[0], path[1]) == "OK" else speak(
+                                ENGINE, "Cannot copy file because of error")
                         elif path[0] == "Folder into file":
-                            speak("Seriously? Do you want to copy folder to file? Think about it")
+                            speak(ENGINE, "Seriously? Do you want to copy folder to file? Think about it")
                         elif path[0] == "Not a folder":
-                            speak("Selected item must be a folder, but not file")
+                            speak(ENGINE, "Selected item must be a folder, but not file")
             for phrase in TIME_NOW_STRS:
                 if phrase in text:
-                    speak(f"Current time is {datetime.datetime.now().strftime('%H:%M')}")
+                    speak(ENGINE, f"Current time is {datetime.datetime.now().strftime('%H:%M')}")
 
             for phrase in TIMER_STRS:
                 if phrase in text:
                     timer_thread = threading.Thread(target=set_timer, args=(text,))
                     timer_thread.start()
+            for phrase in OPEN_PROGRAM_STRS:
+                if phrase in text:
+                    print(phrase, text)
+                    program_name = text.replace(phrase, '')
+                    open_program(program_name)
 
         for phrase in STOP:
             if phrase in text:
                 to_stop.append(phrase)
         if len(to_stop) > 0:
-            speak(random.choice(END_STR))
+            speak(ENGINE, random.choice(END_STR))
             break
 
 
 if __name__ == '__main__':
+    get_info_before_begin()
     print("Mark's listening...")
     main()

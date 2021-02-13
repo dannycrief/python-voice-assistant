b88b46e5e5f60b060315f8e78bdf70ee0221d1c6
Stepan Kozurak <step.kozbvb@icloud.com>
2021-02-13 21:22:30 +0100

    Added logging

--- additional_functions/before_start.py
+++ additional_functions/before_start.py
@@ -2,6 +2,7 @@ import re
 import pickle
 import getpass
 import os.path
+import logging
 from random import choices
 
 from additional_functions.VA_config import speak, get_speak_engine
@@ -10,6 +11,7 @@ ENGINE = get_speak_engine()
 
 
 def get_info_before_begin():
+    logging.info('Getting user information before begin')
     installed_apps = get_installed_apps_before_begin()
     user_info = get_user_info_before_begin()
     return installed_apps, user_info
@@ -39,17 +41,21 @@ def get_installed_programs():
 
 
 def get_installed_apps_before_begin():
+    logging.info('Getting installed apps')
     installed_apps = None
     if os.path.exists('installed_programs.pickle'):
+        logging.info('File with installed programs was found')
         with open('installed_programs.pickle', 'rb') as token:
             installed_apps = pickle.load(token)
     if not installed_apps:
+        logging.info('File with installed programs was not found. Getting installed programs')
         speak(ENGINE,
-              "Hello, my name is Mark. I am your virtual voice assistant. Give me few minutes to configure settings")
-        print("Mark starts configuration...")
+              "Hello, my name is Sara. I am your virtual voice assistant. Give me few minutes to configure settings")
+        print("Sara starts configuration...")
         installed_apps = get_installed_programs()
         with open("installed_programs.pickle", "wb") as pickle_file:
             pickle.dump(installed_apps, pickle_file)
+    logging.info('Installed applications was added to pickle file')
     print("Collected data about programs")
     return installed_apps
 
@@ -61,6 +67,7 @@ def get_user_info_before_begin():
             user_info = pickle.load(token)
             speak(ENGINE, "Hello {}!".format(user_info['first_name']))
     if not user_info:
+        logging.info("File with user information was not found")
         print("Getting some information about user for better work")
         speak(ENGINE, "What is your name?")
         first_name = input().capitalize()
@@ -79,13 +86,17 @@ def get_user_info_before_begin():
         }
         with open("user_info.pickle", "wb") as pickle_file:
             pickle.dump(user_info, pickle_file)
+    logging.info("Got user information. File with user information was successfully created")
     print("Collected data about user")
     print("Mark successfully ended configuration...")
     return user_info
 
 
 def is_valid_email(email_address):
+    logging.info("Validation of user email")
     regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
     if re.search(regex, email_address):
+        logging.info("Email is valid")   
         return True
+    logging.info("Email is invalid")
     return False
